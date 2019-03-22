9a9127dc98c8a1237e1ca2fe4a3b0853e80e20e5
Shubham <shubham@logisticinfotech.co.in>
2019-03-22 14:36:57 +0530

    Setup npm

---
+++ HorizontalDateTimePicker/HorizontalDatePickerStyle.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+  },
+  yearTextContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 10,
+  },
+  yearTextStyle: {
+    fontSize: 15,
+    color: 'black',
+  },
+  datePickerContainer: {
+    height: 60,
+    marginVertical: 5,
+  },
+  flatListStyle: {
+    marginHorizontal: 10,
+  },
+  itemViewStyle: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 5,
+    paddingHorizontal: 5,
+    marginHorizontal: 5,
+  },
+  textSelected: {
+    fontSize: 15,
+    color: 'steelblue',
+  },
+  textUnSelected: {
+    fontSize: 15,
+    color: 'gray',
+  },
+});
+
+export default styles;
