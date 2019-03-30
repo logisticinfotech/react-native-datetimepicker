1eb2dd934d2ce6ede04f1b8d7a2afd027672c696
Shubham <shubham@logisticinfotech.co.in>
2019-03-30 15:57:13 +0530

    example display design change

--- example/DateTimePicker/App.js
+++ example/DateTimePicker/App.js
@@ -10,35 +10,48 @@ import React, { Component } from 'react';
 import { StyleSheet, View, Text, SafeAreaView } from 'react-native';
 
 import HorizontalDatePicker from '@logisticinfotech/react-native-horizontal-date-picker';
+import moment from 'moment';
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      date: '',
-      time: '',
-      dateTime: null,
+      date1: '',
+      time1: '',
+      dateTime1: '',
+      date2: '',
+      time2: '',
+      dateTime2: '',
+      date3: '',
+      time3: '',
+      dateTime3: '',
     };
   }
 
   onDateSelected = date => {
     this.setState({
-      date: date,
+      date1: date,
+      date2: moment(date, 'DD-MM-YYYY').format('DD MMM YYYY'),
+      date3: moment(date, 'DD-MM-YYYY').format('Do MMMM YYYY'),
     });
   };
 
   onTimeSelected = time => {
     this.setState({
-      time: time,
+      time1: time,
+      time2: moment(time, 'hh:mm a').format('HH:mm'),
+      time3: moment(time, 'hh:mm a').format('HH:mm a'),
     });
   };
 
   onDateTimeSelected = datetime => {
     this.setState({
-      dateTime: datetime.datetime,
+      dateTime1: datetime.datetime,
+      dateTime2: moment(datetime.datetime, 'DD-MM-YYYY hh:mm a').format('DD MMM YYYY HH:mm'),
+      dateTime3: moment(datetime.datetime, 'DD-MM-YYYY hh:mm a').format('Do MMMM YYYY [@] hh:mm a'),
     });
   };
   render() {
-    const { date, time, dateTime } = this.state;
+    const { date1, date2, date3, time1, time2, time3, dateTime1, dateTime2, dateTime3 } = this.state;
     return (
       <SafeAreaView style={styles.container}>
         <HorizontalDatePicker
@@ -49,16 +62,19 @@ export default class App extends Component {
         <View style={styles.gapStyle} />
 
         <View style={styles.displayContainer}>
-          <Text style={styles.title}>Date:</Text>
-          <Text style={styles.instructions}>{date}</Text>
+          <Text style={[styles.instructions, styles.textColor1]}>{date1}</Text>
+          <Text style={[styles.instructions, styles.textColor1]}>{time1}</Text>
+          <Text style={[styles.instructions, styles.textColor1]}>{dateTime1}</Text>
         </View>
         <View style={styles.displayContainer}>
-          <Text style={styles.title}>Time:</Text>
-          <Text style={styles.instructions}>{time}</Text>
+          <Text style={[styles.instructions, styles.textColor2]}>{date2}</Text>
+          <Text style={[styles.instructions, styles.textColor2]}>{time2}</Text>
+          <Text style={[styles.instructions, styles.textColor2]}>{dateTime3}</Text>
         </View>
         <View style={styles.displayContainer}>
-          <Text style={styles.title}>DateTime:</Text>
-          <Text style={styles.instructions}>{dateTime}</Text>
+          <Text style={[styles.instructions, styles.textColor3]}>{date3}</Text>
+          <Text style={[styles.instructions, styles.textColor3]}>{time3}</Text>
+          <Text style={[styles.instructions, styles.textColor3]}>{dateTime3}</Text>
         </View>
       </SafeAreaView>
     );
@@ -75,22 +91,23 @@ const styles = StyleSheet.create({
     height: 50,
   },
   displayContainer: {
-    flexDirection: 'row',
     marginVertical: 10,
     marginHorizontal: 15,
   },
-  title: {
-    width: '25%',
-    color: 'red',
-    textAlign: 'left',
-    fontSize: 15,
-    fontWeight: 'bold',
-  },
   instructions: {
     width: '75%',
-    color: 'steelblue',
     textAlign: 'left',
     fontSize: 15,
+    marginVertical: 2,
     fontWeight: 'bold',
+  },
+  textColor1: {
+    color: '#0d47a1',
+  },
+  textColor2: {
+    color: '#004d40',
+  },
+  textColor3: {
+    color: '#d84315',
   },
 });
